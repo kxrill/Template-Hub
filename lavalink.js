3deccbe47d77fa49f1a3c7d7bceb01f864dc1ec2
kxrill <kklewzow@gmail.com>
2026-03-12 20:45:24 +0100

    Add files via upload

---
+++ lavalink.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    enabled: true, 
+    lavalink: {
+            name: "GlaceYT",
+            password: "glace",
+            host: "ge-02.vortexa.cloud",
+            port: 11050,
+            secure: false
+    }
+};           
+
+
+
+
